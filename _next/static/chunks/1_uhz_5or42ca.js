61a446d44f1168e395df4c5bbf6a831069d779f5
bpollak <bpollak@users.noreply.github.com>
2026-06-18 22:25:44 +0000

    deploy: be707228107b01fadcff89bc2a82b9be0539b588

--- _next/static/chunks/1_uhz_5or42ca.js
+++ _next/static/chunks/1_uhz_5or42ca.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/2h9h_zjbe11zz.js",
   "static/chunks/206ys45k35d1v.js",
   "static/chunks/3wrs0vnvhw5i0.js",
-  "static/chunks/1jqglyzm10xfy.css",
-  "static/chunks/turbopack-2361tujvjuqiz.js"
+  "static/chunks/43yznira07mtd.css",
+  "static/chunks/turbopack-3prg0h0_psnqa.js"
 ])
