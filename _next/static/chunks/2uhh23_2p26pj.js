9bb7bf60900e3a2a80819675d8ed1dcc9fed3f9d
bpollak <bpollak@users.noreply.github.com>
2026-06-18 23:37:07 +0000

    deploy: b721b0133017afaf3df1c27fb299acdb7109c4af

--- _next/static/chunks/2uhh23_2p26pj.js
+++ _next/static/chunks/2uhh23_2p26pj.js
@@ -2,6 +2,6 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/2h9h_zjbe11zz.js",
   "static/chunks/206ys45k35d1v.js",
   "static/chunks/3wrs0vnvhw5i0.js",
-  "static/chunks/3ef9p7r_zk-la.css",
-  "static/chunks/turbopack-37_g5m41dd61t.js"
+  "static/chunks/1bu8owvk9_kqx.css",
+  "static/chunks/turbopack-3y7ccw8yizk6e.js"
 ])
